fa947ca2f1b3aa6416183d2e5e0255df37b30782
RilanRMZ <rilan.rmz@gmail.com>
2025-06-28 10:18:41 +0400

    added cors policy

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://doctor-appoinment-app-zeta.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
